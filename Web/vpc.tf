80920eedeedc47f814d4a304b84b10d3c955b8df
bmokase3093 <bmokase3093@gmail.com>
2022-10-30 15:52:30 +0200

    create a VPC

---
+++ Web/vpc.tf
@@ -0,0 +1,40 @@
+# create a VPC/Network where we want to deploy our resources
+resource "aws_vpc" "Nginx-Vpc" {
+    cidr_block           = "10.0.0.0/16"
+    enable_dns_hostnames = "true"
+    instance_tenancy     = "default"
+
+    tags = {
+        Name = "Nginx_VPC"
+    }
+}
+
+# # Create single public subnet
+# resource "aws_subnet" "Nginx-PubSub" {
+#     vpc_id                  = aws_vpc.Nginx-Vpc.id
+#     cidr_block              = "10.0.1.0/24"
+#     map_public_ip_on_launch = "true"
+
+#     tags = {
+#         Name = "Nginx_PubSub"
+#     }
+# }
+
+# # create an Internet Gateway for above VPC
+# resource "aws_internet_gateway" "Nginx-IGW" {
+#     vpc_id = aws_vpc.Nginx-Vpc.id
+
+#     tags = {
+#        Name = "Nginx_IGW"
+#     }
+# }
+
+# # create route table for VPC
+# resource "aws_route_table" "Nginx-PubSub-RT" {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.Nginx_IGW
+
+#     tags = {
+#        Name = "Nginx_PubSubRT"
+#     }
+# }
